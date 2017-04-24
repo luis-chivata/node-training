f26b67adc6f9f1a96a993b82d00edeb27c3235cb
Luis Carlos Chivata <luis.chivata@zemoga.com>
2017-04-24 15:38:42 -0500

    Fourth example: Interactive IMC calculator

---
+++ imc-interactive-calculator/utils/imc.js
@@ -0,0 +1,34 @@
+var chalk = require('chalk');
+
+function imc_description(weight,height) {
+    //height = height / 100;
+    var imc = weight / Math.pow(height,2);
+    if (imc < 16) {
+        return chalk.red('Infrapeso: Delgadez Severa');
+    } else if (imc < 16.99) {
+        return chalk.red('Infrapeso: Delgadez Moderada');
+    } else if (imc < 18.49) {
+        return chalk.orange('Infrapeso: Delgadez Aceptable');
+    } else if (imc < 24.99) {
+        return chalk.green('Peso Normal');
+    } else if (imc < 29.99) {
+        return chalk.yellow('Sobrepeso');
+    } else if (imc < 34.99) {
+        return chalk.red('Obeso: Tipo I');
+    } else if (imc < 39.99) {
+        return chalk.red('Obeso: Tipo II');
+    } else {
+        return chalk.red('Obeso: Tipo III');
+    }
+
+}
+
+function imc(weight,height) {
+     var imc = weight / Math.pow(height,2);
+     return imc;
+}
+
+module.exports = {
+    description: imc_description,
+    value: imc
+};
