9eb6204fc3e00a6cd5d307ef34afcfe0eeb30c1b
Luis Carlos Chivata <luis.chivata@zemoga.com>
2017-05-08 14:30:51 -0500

    Adding API mount point

--- using-express/server/index.js
+++ using-express/server/index.js
@@ -1,6 +1,11 @@
 "use strict";
 
 const express = require('express');
+const morgan = require('morgan');
+const logger = require('winston');
+
+const api = require("./api");
+
 const app = express();
 
 const requestTime = function(req, res, next) {
@@ -8,15 +13,19 @@ const requestTime = function(req, res, next) {
     next();
 }
 
-app.use(requestTime);
+app.use(morgan('common'));
+
+app.use("/api",api);
 
 app.get('/', (req, res) => {
+    logger.info("Home Page")
     res.json({
         "message": "Welcome to the API"
     });
 });
 
 app.use((req, res, next) => {
+    logger.info("Route not found");
     res.status(400);
     res.json({
         "error":"Error, Route not found"
@@ -24,6 +33,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
+    logger.error("Error");
     res.status(500);
     res.json({
         "error": `${err}`
