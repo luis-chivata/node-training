b8ad054f1462aa369375be3e7bc333fee1b1efd2
Luis Carlos Chivata <luis.chivata@zemoga.com>
2017-05-08 09:45:22 -0500

    Added error handling middleware

--- using-express/server/index.js
+++ using-express/server/index.js
@@ -3,8 +3,31 @@
 const express = require('express');
 const app = express();
 
+const requestTime = function(req, res, next) {
+    req.requestTime = Date.now();
+    next();
+}
+
+app.use(requestTime);
+
 app.get('/', (req, res) => {
-    res.send('Hello World\n');
+    res.json({
+        "message": "Welcome to the API"
+    });
+});
+
+app.use((req, res, next) => {
+    res.status(400);
+    res.json({
+        "error":"Error, Route not found"
+    });
+});
+
+app.use((err, req, res, next) => {
+    res.status(500);
+    res.json({
+        "error": `${err}`
+    });
 });
 
 module.exports = app;
